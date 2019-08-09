1d6ecfb0b2f61245ce3b4c34e634eeb9ed496282
jbride <jbride2001@yahoo.com>
2019-08-09 17:20:11 -0600

    casemgmt_state_refresh.sh

--- casemgmt_state_refresh.sh
+++ casemgmt_state_refresh.sh
@@ -5,7 +5,7 @@ sso_project=rhsso-sso0
 
 enableLetsEncryptCertsOnRoutes() {
     oc new-project prod-letsencrypt
-    oc create -fhttps://raw.githubusercontent.com/tnozicka/openshift-acme/master/deploy/letsencrypt-live/cluster-wide/{clusterrole,serviceaccount,imagestream,deployment}.yaml -n prod-letsencrypt
+    oc create -fhttps://raw.githubusercontent.com/gpe-mw-training/openshift-acme/master/deploy/letsencrypt-live/cluster-wide/{clusterrole,serviceaccount,imagestream,deployment}.yaml -n prod-letsencrypt
     oc adm policy add-cluster-role-to-user openshift-acme -z openshift-acme -n prod-letsencrypt
 
     echo -en "metadata:\n  annotations:\n    kubernetes.io/tls-acme: \"true\"" > /tmp/route-tls-patch.yml
