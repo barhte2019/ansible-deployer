c1f4421775ceb0d5a99f210765d3de896a5feac5
Diego Alejandro Torres Fuerte <diego-torres@users.noreply.github.com>
2019-09-16 17:21:55 -0500

    Update set_env_vars.sh

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -9,7 +9,7 @@ echo "export OCP_WILDCARD_DOMAIN=apps-\$OCP_DOMAIN" >> ~/.bashrc
 echo "export rhsso_url=sso-rhsso-sso0.\$OCP_WILDCARD_DOMAIN" >> ~/.bashrc
 echo "export bc_url=rhpam-bc-rhpam-dev-\$OCP_USERNAME.\$OCP_WILDCARD_DOMAIN" >> ~/.bashrc
 echo "export ks_url=rhpam-kieserver-rhpam-dev-\$OCP_USERNAME.\$OCP_WILDCARD_DOMAIN" >> ~/.bashrc
-echo "export fuse_url=document-integration-swift-integration.\$OCP_WILDCARD_DOMAIN
+echo "export fuse_url=document-integration-swift-integration.\$OCP_WILDCARD_DOMAIN" >> ~/.bashrc
 
 echo "export RHPAM_PROJECT=rhpam-dev-\$OCP_USERNAME" >> ~/.bashrc
 echo "export RHPAM_TOOLS_PROJECT=pam-7-tools-\$OCP_USERNAME" >> ~/.bashrc
